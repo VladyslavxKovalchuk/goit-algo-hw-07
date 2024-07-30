0462d55d84c5940225eaa1edc9a2e91fa6db28f1
Vladyslav Kovalchuk <vladkovalchuk09@gmail.com>
2024-07-30 23:12:37 +0300

    Домашнє завдання до теми “Дерева та балансування”

---
+++ task2/main.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+    def __str__(self, level=0, prefix="Root: "):
+        ret = "\t" * level + prefix + str(self.val) + "\n"
+        if self.left:
+            ret += self.left.__str__(level + 1, "L--- ")
+        if self.right:
+            ret += self.right.__str__(level + 1, "R--- ")
+        return ret
+
+    def find_min(self):
+        if self.left is None:
+            return self.val
+        return self.left.find_min()
+
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if key < root.val:
+            root.left = insert(root.left, key)
+        else:
+            root.right = insert(root.right, key)
+    return root
+
+
+root = Node(5)
+root = insert(root, 3)
+root = insert(root, 2)
+root = insert(root, 4)
+root = insert(root, 7)
+root = insert(root, 6)
+root = insert(root, 8)
+
+print(root)
+print(f'Найменше значення: {root.find_min()}')
+
